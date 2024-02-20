54e2043a2a124ec7d05c1899428f1cf18c93e534
Zamius <costi19000@gmail.com>
2024-02-20 23:22:46 +0200

    Added useful gems and divided "monitoring" features into 3 main classes

--- Gemfile
+++ Gemfile
@@ -4,3 +4,5 @@ source "https://rubygems.org"
 
 gem 'sys-proctable', '~> 1.2', '>= 1.2.6'
 gem 'awesome_print'
+gem 'sys-filesystem'
+gem "free_disk_space"
--- rubysysmon.rb
+++ rubysysmon.rb
@@ -1,6 +1,41 @@
 require 'sys/proctable'
+require 'sys/filesystem'
+require 'free_disk_space'
 require 'awesome_print'
+require 'optparse'
 
-Sys::ProcTable.ps{ |process|
-   ap process
-}
+#Sys::ProcTable.ps{ |process|
+ #  ap process
+#}
+
+# Disk stuff
+class DISKMON
+   attr_reader :used
+   def free_disk_space
+      @used = FreeDiskSpace.gigabytes("/")
+   end
+
+   def total_disk_space
+      total = Sys::Filesystem.stat("/")
+      return total.bytes_total / 1000000000
+   end
+end
+
+# Processes
+class PROCMON
+
+end
+
+# Network...
+class NETMON
+
+end
+
+def main
+   disk = DISKMON.new()
+
+   ap "Free disk space: #{disk.free_disk_space.to_f.round(3)} GB"
+   ap "Total available disk space: #{disk.total_disk_space} GB"
+end
+
+main
