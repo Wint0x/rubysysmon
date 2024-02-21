51cc3f46130958beb8bfc075edb9e8d0af24c447
Zamius <costi19000@gmail.com>
2024-02-21 08:09:21 +0200

    Added mounts enumeration and file count

--- rubysysmon.rb
+++ rubysysmon.rb
@@ -10,15 +10,32 @@ require 'optparse'
 
 # Disk stuff
 class DISKMON
-   attr_reader :used
+   attr_reader :space_used
+   attr_reader :total_space
+   attr_reader :total_files
+   attr_reader :mounts
+
    def free_disk_space
-      @used = FreeDiskSpace.gigabytes("/")
+      @space_used = FreeDiskSpace.gigabytes("/")
    end
 
    def total_disk_space
-      total = Sys::Filesystem.stat("/")
-      return total.bytes_total / 1000000000
+      @total_space = Sys::Filesystem.stat("/")
+      return @total_space.bytes_total / 1000000000
    end
+
+   def get_files_count
+      @total_files = Sys::Filesystem.stat("/")
+      return @total_files.files
+   end
+
+   # mounts
+   def get_mounts
+      @mounts = Sys::Filesystem.mounts.each_with_index do 
+         | mount, idx | puts ("[#{(idx + 1).to_s}] " + mount.name + "\n" + mount.mount_point + "\n\n")
+      end
+   end
+
 end
 
 # Processes
@@ -36,6 +53,10 @@ def main
 
    ap "Free disk space: #{disk.free_disk_space.to_f.round(3)} GB"
    ap "Total available disk space: #{disk.total_disk_space} GB"
+   puts "Total files encountered: \033[1m\033[36m#{disk.get_files_count}\033[0m"
+   puts "\n=== [ Mounts ] ==="
+   disk.get_mounts
+
 end
 
 main
